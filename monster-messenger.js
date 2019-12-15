efa5d71477130cfbb783efd1dbb98d2981420a3f
Kullen Wetherby <wetherbyk71@gmail.com>
2019-12-15 15:27:18 -0500

    passed all tests

---
+++ monster-messenger.js
@@ -0,0 +1,16 @@
+const monsterData = require('./christmas-monsters.json')
+const randomArrayIndex = require('random-array-index')
+
+function getRandomMessage () {
+    const monsterIdx = randomArrayIndex(monsterData)
+    const monster = monsterData[monsterIdx]
+    // needs to get a random statement
+    const statement = monster.scaryStatements[0]
+
+    return statement
+}
+
+module.exports = {
+    getRandomMessage,
+    monsterData
+}
---
+++ test/tests.js
@@ -0,0 +1,38 @@
+var mocha = require('mocha')
+var expect = require('chai').expect
+var { describe, it } = mocha
+
+// include the module under test - monster-messenger
+const messenger = require('../monster-messenger')
+
+// TDD - Test driven development
+describe('Christmas Monster text message generator',  () => {
+    it('generates a scary text for all monsters',  () => {
+        // given we execute the function
+        let message = messenger.getRandomMessage()
+        let statementFound = false;
+        
+        // find out if there is a match
+        messenger.monsterData.forEach( (monster) => {
+            const monsterMatches = monster.scaryStatements.filter( (statement) => {
+                return statement === message
+            } )
+
+            if (monsterMatches.length > 0) {
+                statementFound = true;
+            }
+
+        } )
+
+        // we expect at least one match
+        expect(statementFound).to.equal(true)
+    })
+
+    it('does not send more than one text per hour',  () => {
+    })
+
+    it('does not send messages from blacklisted monsters', () => {
+
+    })
+})
+
